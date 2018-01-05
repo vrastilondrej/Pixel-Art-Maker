9d6c22ca92fbe0f95f36de5341416281383c6797
Ondřej Vraštil <vrastilondrej@gmail.com>
2018-01-05 05:00:49 +0100

    Edit

--- designs.js
+++ designs.js
@@ -1,10 +1,39 @@
 // Select color input
-// Select size input
+var colorPicker; //colorPicker slector
+var selectedColor; // choosen color value
+var gridHeight = 1; //grid height value
+var gridWidth = 1; //grid width value
+var pixelCanvas //grid table
+colorPicker = $("#colorPicker");
+pixelCanvas = $("#pixel_canvas");
+
+//If the input value change, then refresh variables value
+$("input").on("change", function() {
+    selectedColor = colorPicker.val();
+    gridHeight = $("#input_height").val();
+    gridWidth = $("#input_width").val();
+});
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+$("#sizePicker").submit(function makeGrid(evt) {
 
-}
+    //Do stuff here
+
+    evt.preventDefault();
+    $("tr").remove();
+    for (let i = 0; i < gridHeight; i++) {
+        pixelCanvas.append("<tr><td></td></tr>");
+    }
+
+    $("tr").each(function() {
+        for (let j = 1; j < gridWidth; j++) {
+            $(this).append("<td></td>");
+            console.log("posilam form" + j);
+        };
+    });
+    $("td").on("click", function() {
+        $(this).css("background-color", selectedColor);
+    });
+});
